83a758bddfbd714ba90417044fc7d143b13f558f
travis@travestyofcode.com <travis@travestyofcode.com>
2023-10-07 08:23:27 -0400

    Adds ToDo classes and DB Context.

---
+++ ToDoApp/Data/ToDoList.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
+
+namespace ToDoApp.Data;
+
+public class ToDoList
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public List<ToDoItem> Items { get; set; }
+}
+
+public class ToDoListConfiguration : IEntityTypeConfiguration<ToDoList>
+{
+    public void Configure(EntityTypeBuilder<ToDoList> builder)
+    {
+        builder.ToTable(nameof(ToDoList));
+
+        builder.Property(p => p.Title)
+            .IsRequired(true)
+            .HasMaxLength(64);
+    }
+}
