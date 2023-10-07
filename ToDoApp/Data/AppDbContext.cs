4accdadfc984d3392b8af0ab0f1d54f9ec5812e6
TravestyOfCode <78275281+TravestyOfCode@users.noreply.github.com>
2023-10-07 08:31:09 -0400

    Data (#1)
    * Adds EFCore with SqlServer packages.
    
    * Adds ToDo classes and DB Context.
    
    * Adds default connection string for database.
    
    * Adds justfile for applying data migrations.
    
    * Adds EFCore Design package for migration support.
    
    * Includes DB Context configuration for program.
    
    * Adds initial data migration.

---
+++ ToDoApp/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApp.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<ToDoItem> ToDoItems { get; set; }
+
+    public DbSet<ToDoList> ToDoLists { get; set; }
+
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+    }
+}
---
+++ ToDoApp/Data/ToDoItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ToDoApp.Data;
+
+public class ToDoItem
+{
+    public int Id { get; set; }
+
+    public int ToDoListId { get; set; }
+
+    public ToDoList ToDoList { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public string Description { get; set; }
+}
+
+public class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItem>
+{
+    public void Configure(EntityTypeBuilder<ToDoItem> builder)
+    {
+        builder.ToTable(nameof(ToDoItem));
+
+        builder.Property(p => p.Description)
+            .IsRequired(true);
+    }
+}
