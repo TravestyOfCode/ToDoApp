cc918e19ee5bf6b43cd206bdf8d7a144d1be5816
travis@travestyofcode.com <travis@travestyofcode.com>
2023-10-10 01:43:20 -0400

    Adds List, Create, Edit, and Delete functions for ToDoItem with associated Views.

--- ToDoApp/Controllers/HomeController.cs
+++ ToDoApp/Controllers/HomeController.cs
@@ -50,6 +50,164 @@ public class HomeController : Controller
         return View();
     }
 
+    // Updates a single ToDoItem's IsCompleted property. If isCompleted is 'on' then it is set to true, otherwise false.
+    [HttpPost]
+    public async Task<IActionResult> UpdateStatus(int id, string isCompleted = "", CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var toDoItem = await _dbContext.ToDoItems.Where(tdi => tdi.Id.Equals(id)).SingleOrDefaultAsync(cancellationToken);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            toDoItem.IsCompleted = isCompleted.Equals("on");
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error updating ToDoItem status.");
+            return StatusCode(500);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var item = await _dbContext.ToDoItems.Where(tdi => tdi.Id.Equals(id)).SingleOrDefaultAsync(cancellationToken);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("EditToDoItemPartial", item);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error editing a ToDoItem.");
+            return StatusCode(500);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, string description, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var toDoItem = await _dbContext.ToDoItems.SingleOrDefaultAsync(tdi => tdi.Id.Equals(id), cancellationToken);
+
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            toDoItem.Description = description;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return PartialView("ToDoItemPartial", toDoItem);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error saving ToDoItem changes.");
+
+            return StatusCode(500);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Cancel(int id, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var toDoItem = await _dbContext.ToDoItems.SingleOrDefaultAsync(tdi => tdi.Id.Equals(id), cancellationToken);
+
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("ToDoItemPartial", toDoItem);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error canceling editing of ToDoItem.");
+            return StatusCode(500);
+        }
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var toDoItem = await _dbContext.ToDoItems.SingleOrDefaultAsync(tdi => tdi.Id.Equals(id), cancellationToken);
+
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.ToDoItems.Remove(toDoItem);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return StatusCode(200);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error trying to delete ToDoItem.");
+            return StatusCode(500);
+        }
+    }
+
+    [HttpGet]
+    public IActionResult Create(int listId, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            return PartialView("CreateToDoItemPartial", listId);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error creating new ToDoItem line.");
+            return StatusCode(500);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(int listId, string description, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var listEntity = await _dbContext.ToDoLists.SingleOrDefaultAsync(tdl => tdl.Id.Equals(listId), cancellationToken);
+
+            if (listEntity == null)
+            {
+                return NotFound();
+            }
+
+            var entity = _dbContext.ToDoItems.Add(new ToDoItem() { ToDoListId = listId, Description = description, ToDoList = listEntity });
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return PartialView("SavedToDoItemPartial", entity.Entity);
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error saving new ToDoItem.");
+            return StatusCode(500);
+        }
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
